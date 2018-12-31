b119a560e9097b9ef89674f8ae2efc50d690474c
Ran Tavory <rantav@gmail.com>
2018-12-31 12:02:48 +0200

    Modify k8s to use a single broker kafka cluster

--- k8s/kafka-destination/test.sh
+++ k8s/kafka-destination/test.sh
@@ -18,7 +18,7 @@ fi
 kubectl --context eu-west-1.k8s.local exec -n kafka-destination pzoo-destination-0 -- /opt/kafka/bin/zookeeper-shell.sh localhost:2181 get /brokers/ids/0
 
 # Test Kafka from the inside
-until POD=$(kubectl --context eu-west-1.k8s.local -n kafka-destination get po kafka-destination-1 | grep Running)
+until POD=$(kubectl --context eu-west-1.k8s.local -n kafka-destination get po kafka-destination-0 | grep Running)
 do
   echo "KAFKA on destination isn't ready yet"
   sleep 20
@@ -28,4 +28,4 @@ if [ $FIRST_TIME ]; then
     sleep 60
 fi
 kubectl --context eu-west-1.k8s.local exec -n kafka-destination kafka-destination-0 -- bash -c "unset JMX_PORT; echo '>>>>>>>>>>>>>  GREAT SUCCESS! <<<<<<<<<<<<<<<<' | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test_topic_2"
-kubectl --context eu-west-1.k8s.local exec -n kafka-destination kafka-destination-1 -- bash -c "unset JMX_PORT; /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --from-beginning --topic test_topic_2 --max-messages 1"
+kubectl --context eu-west-1.k8s.local exec -n kafka-destination kafka-destination-0 -- bash -c "unset JMX_PORT; /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --from-beginning --topic test_topic_2 --max-messages 1"
