1fcd8441a159a5c9f27c942635b58249fea6b884
Ran Tavory <rantav@gmail.com>
2019-01-01 16:12:42 +0200

    The tabs didn't work. Use spaces instead...

--- k8s/kafka-destination/test.sh
+++ k8s/kafka-destination/test.sh
@@ -27,5 +27,5 @@ done
 if [ $FIRST_TIME ]; then
     sleep 60
 fi
-kubectl --context eu-west-1.k8s.local exec -n kafka-destination kafka-destination-0 -- bash -c "unset JMX_PORT; echo '\t\t >>>>>>>>>>>>>  GREAT SUCCESS! <<<<<<<<<<<<<<<<' | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test_topic_2"
+kubectl --context eu-west-1.k8s.local exec -n kafka-destination kafka-destination-0 -- bash -c "unset JMX_PORT; echo '                                 >>>>>>>>>>>>>  GREAT SUCCESS! <<<<<<<<<<<<<<<<' | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test_topic_2"
 kubectl --context eu-west-1.k8s.local exec -n kafka-destination kafka-destination-0 -- bash -c "unset JMX_PORT; /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --from-beginning --topic test_topic_2 --max-messages 1"
